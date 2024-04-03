4cc770059f9e34a57295424854b4d59d9d630341
Shrish KS <shrishdks@gmail.com>
2024-04-03 13:22:47 +0530

    Added signin

--- lib/screens/Authentication/signin.dart
+++ lib/screens/Authentication/signin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lockbox/shared/constants.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -8,10 +9,111 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  String email="";
+  String pass="";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text("de"),
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/gradient.png"),
+                  fit: BoxFit.cover
+              )
+          ),
+          child: Container(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    "Welcome Back!",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 0.9,
+                      wordSpacing: 1
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(25))
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 25,vertical: 20),
+                    child: Form(
+                      key: _formkey,
+                      child: Column(
+                        children: [
+                          SizedBox(height: 20,),
+                          Container(
+                            child: TextFormField(
+                              decoration: textinputdecoration.copyWith(hintText: "Email/Phone",prefixIcon: Icon(Icons.person),label: Text("Email/Phone")),
+                              validator: (val) => val!.length==0?"Email cant be empty":null,
+                              onChanged: (val) =>{
+                                email=val
+                              },
+                              style: TextStyle(
+                                fontSize: 20,
+                                letterSpacing: 0.5,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20,),
+                          Container(
+                            child: TextFormField(
+                              decoration: textinputdecoration.copyWith(hintText: "Password",prefixIcon: Icon(Icons.key),label: Text("Password")),
+                              validator: (val) => val!.length<6?"Password should be atleast 6 charecters":null,
+                              onChanged: (val) =>{
+                                pass=val
+                              },
+                              style: TextStyle(
+                                fontSize: 20,
+                                letterSpacing: 0.5,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 150,
+                            child: TextButton(
+                              style: ButtonStyle(
+
+                                side: MaterialStatePropertyAll(BorderSide()),
+                                backgroundColor: MaterialStatePropertyAll(Color.fromRGBO(
+                                    90, 1, 91, 1.0)),
+                                foregroundColor: MaterialStatePropertyAll(Colors.red),
+                              ),
+                                onPressed: (){
+                                  if(_formkey.currentState!.validate()){
+                                    print(email);
+                                    print(pass);
+                                  }
+                                },
+                                child: Text("Login")
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        )
+      ),
     );
   }
 }
---
+++ lib/shared/constants.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+final textinputdecoration = InputDecoration(
+  contentPadding: EdgeInsets.symmetric(vertical: 0,horizontal: 10),
+  filled: true,
+  fillColor: Colors.grey[200],
+  prefixIconColor: MaterialStateColor.resolveWith((states) =>
+  states.contains(MaterialState.focused)
+      ? Colors.purple
+      : Colors.grey),
+  floatingLabelStyle: TextStyle(
+    color: Colors.purple[900]
+  ),
+  focusedErrorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+      borderSide: BorderSide(
+        color: Colors.red[300]??Colors.red,
+        width: 2,
+      )
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(20)),
+      borderSide: BorderSide(
+        color: Colors.grey,
+        width: 2,
+      )
+  ),
+  focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+      borderSide: BorderSide(
+        color: Colors.purple,
+        width: 2,
+      )
+  ),
+);
+
+final inputcontainerdecoration= BoxDecoration(
+color: Colors.grey[300],
+borderRadius: BorderRadius.all(Radius.circular(20))
+);
